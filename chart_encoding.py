130a974ad97b8b1917076deaee2b80a3549ffad1
olivier.dagenais <olivier.dagenais@gmail.com>
2010-03-18 20:17:04 +0000

    1 - Refactored for consistency: renamed a few variables, parameters and constants.
    2 - Restructured both *Encode() methods for consistency and to fix edge case bugs:  non-numbers, values less than zero and values greater than maxValue now map to the underscore.

--- chart_encoding.py
+++ chart_encoding.py
@@ -15,44 +15,48 @@ def is_number(s):
 
 simpleEncoding = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 simpleEncodingLength = len(simpleEncoding)
+simpleRange = simpleEncodingLength - 1
 """
 This function scales the submitted values so that
-maxVal becomes the highest value.
+maxValue becomes the highest value.
 """
-def simpleEncode(valueArray,maxValue):
+def simpleEncode(valueArray, maxValue):
     chartData = ['s:']
     for currentValue in valueArray:
         if is_number(currentValue) and currentValue >= 0:
-            chartData.append(simpleEncoding[int(round((simpleEncodingLength - 1) * currentValue / maxValue))])
-        else:
-            chartData.append('_')
+            # Scale the value to maxValue
+            scaledVal = round(simpleRange * currentValue / maxValue)
+
+            if scaledVal <= simpleRange:
+                chartData.append(simpleEncoding[int(scaledVal)])
+                continue
+
+        chartData.append('_')
 
     return string.join(chartData, '')
 
 
-EXTENDED_MAP = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-.'
-EXTENDED_MAP_LENGTH = len(EXTENDED_MAP)
-EXTENDED_RANGE = EXTENDED_MAP_LENGTH * EXTENDED_MAP_LENGTH - 1
+extendedEncoding = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-.'
+extendedEncodingLength = len(extendedEncoding)
+extendedRange = extendedEncodingLength * extendedEncodingLength - 1
 """
 Same as simple encoding, but for extended encoding.
 """
-def extendedEncode(arrVals, maxVal):
+def extendedEncode(valueArray, maxValue):
     chartData = 'e:'
-    for numericVal in arrVals:
+    for currentValue in valueArray:
+        if is_number(currentValue) and currentValue >= 0:
+            # Scale the value to maxValue
+            scaledVal = round(extendedRange * currentValue / maxValue)
 
-        # Scale the value to maxVal.
-        scaledVal = round(EXTENDED_RANGE * numericVal/maxVal)
+            if scaledVal <= extendedRange:
+                # Calculate first and second digits and add them to the output.
+                quotient = math.floor(scaledVal / extendedEncodingLength)
+                remainder = scaledVal - extendedEncodingLength * quotient
+                chartData += extendedEncoding[int(quotient)] + extendedEncoding[int(remainder)]
+                continue
 
-        if scaledVal > EXTENDED_RANGE or scaledVal > maxVal:
-            scaledVal = maxVal
-        elif scaledVal < 0:
-            chartData += '__'
-            continue
-        else:
-            # Calculate first and second digits and add them to the output.
-            quotient = math.floor(scaledVal / EXTENDED_MAP_LENGTH)
-            remainder = scaledVal - EXTENDED_MAP_LENGTH * quotient
-            chartData += EXTENDED_MAP[int(quotient)] + EXTENDED_MAP[int(remainder)]
+        chartData += '__'
 
     return chartData
 
--- chart_encoding_tests.py
+++ chart_encoding_tests.py
@@ -13,14 +13,16 @@ class TestGlobalFunctions(unittest.TestCase):
         self.assertFalse(chart_encoding.is_number("4a"))
 
     def testsimpleEncode(self):
+        self.assertEqual("s:", chart_encoding.simpleEncode([], 61))
         self.assertEqual("s:ABZabz09", chart_encoding.simpleEncode([0, 1, 25, 26, 27, 51, 52, 61], 61))
         self.assertEqual("s:Monkeys", chart_encoding.simpleEncode([12, 40, 39, 36, 30, 50, 44], 61))
-        self.assertEqual("s:", chart_encoding.simpleEncode([], 61))
+        self.assertEqual("s:_9_", chart_encoding.simpleEncode([-1, 61, 62], 61))
 
     def testextendedEncode(self):
         self.assertEqual("e:", chart_encoding.extendedEncode([], 4095))
         self.assertEqual("e:Monkeys.", chart_encoding.extendedEncode([808, 2532, 1970, 2879], 4095))
         self.assertEqual("e:AAABAZAaAbAz.-..", chart_encoding.extendedEncode([0, 1, 25, 26, 27, 51, 4094, 4095], 4095))
+        self.assertEqual("e:__..__", chart_encoding.extendedEncode([-1, 4095, 4096], 4095))
 
 if __name__ == '__main__':
     unittest.main()
